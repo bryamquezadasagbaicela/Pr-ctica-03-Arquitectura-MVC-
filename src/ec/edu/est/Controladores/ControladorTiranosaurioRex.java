f97b082061762ba2b97afe5c2c46df261341da85
59398 <59398@LAPTOP-32FRB211>
2019-04-24 21:21:07 -0500

    paso6 implementacion del interface Set y la clase HashSet.

--- src/ec/edu/est/Controladores/ControladorTiranosaurioRex.java
+++ src/ec/edu/est/Controladores/ControladorTiranosaurioRex.java
@@ -5,10 +5,54 @@
  */
 package ec.edu.est.Controladores;
 
+import ec.edu.est.Clases.TiranosaurioRex;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author 59398
  */
 public class ControladorTiranosaurioRex {
-    
+   
+    private Set<TiranosaurioRex> lista;
+    private int codigo;
+
+    public ControladorTiranosaurioRex() {
+        lista = new HashSet<>();
+        codigo =0;
+
+    }
+
+    public void create(TiranosaurioRex objeto) {
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);
+    }
+
+    public TiranosaurioRex read(int codigo) {
+        for(TiranosaurioRex tiranosaurioRex : lista){
+            if(tiranosaurioRex.getCodigo()== codigo){
+               return tiranosaurioRex; 
+            }
+        }
+        return null;
+    }
+
+    public void update(TiranosaurioRex objeto) {
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+
+    public void delete(int codigo) {
+        for(TiranosaurioRex tiranosaurioRex : lista){
+            if(tiranosaurioRex.getCodigo()== codigo){
+               lista.remove(tiranosaurioRex);
+               break;
+            }
+        }
+        
+    }
 }
