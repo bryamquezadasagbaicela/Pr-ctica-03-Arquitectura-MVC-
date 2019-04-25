53da689ec2f3e7e2bd2fadb116ff94668a25c904
59398 <59398@LAPTOP-32FRB211>
2019-04-24 21:22:50 -0500

    paso7 implementacion del  interface SortedSet y la clase TreeSet

--- src/ec/edu/est/Controladores/ControladorPezPiedra.java
+++ src/ec/edu/est/Controladores/ControladorPezPiedra.java
@@ -5,10 +5,64 @@
  */
 package ec.edu.est.Controladores;
 
+import ec.edu.est.Clases.PezPiedra;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  *
  * @author 59398
  */
 public class ControladorPezPiedra {
-    
+  
+    private SortedSet<PezPiedra> listaOrdenada;
+    private int codigo;
+
+    public ControladorPezPiedra() {
+        listaOrdenada = new TreeSet<>();
+        codigo =0;
+
+    }
+
+    public void create(PezPiedra objeto) {
+        codigo++;
+        objeto.setCodigo(codigo);
+        listaOrdenada.add(objeto);
+    }
+
+    public PezPiedra read(int codigo) {
+        for(PezPiedra pezPiedra : listaOrdenada){
+            if(pezPiedra.getCodigo()== codigo){
+               return pezPiedra; 
+            }
+        }
+        return null;
+    }
+
+    public void update(PezPiedra objeto) {
+        if(listaOrdenada.contains(objeto)){
+            listaOrdenada.remove(objeto);
+            listaOrdenada.add(objeto);
+        }
+    }
+
+    public void delete(int codigo) {
+        for(PezPiedra pezPiedra : listaOrdenada){
+            if(pezPiedra.getCodigo()== codigo){
+               listaOrdenada.remove(pezPiedra);
+               break;
+            }
+        }
+        
+    }
+
+    public void imprimir(){
+        System.out.println("lista ordenada");
+        for(PezPiedra pezPiedra : listaOrdenada){
+            System.out.println(pezPiedra.getNombre());
+        }
+    }
+
 }
+
+
